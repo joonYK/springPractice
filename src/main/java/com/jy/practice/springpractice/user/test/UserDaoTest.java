1c7b6dcaa1bc117b1146b8f6da8ac56efd9e393e
JY <shutingz@naver.com>
2019-08-04 22:26:10 +0900

    applicationContext가 아닌 @Autowired를 이용해 UserDao를 DI

--- src/main/java/com/jy/practice/springpractice/user/test/UserDaoTest.java
+++ src/main/java/com/jy/practice/springpractice/user/test/UserDaoTest.java
@@ -7,7 +7,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -19,9 +18,6 @@ import static org.junit.Assert.assertThat;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "/applicationContext.xml")
 public class UserDaoTest {
-
-    @Autowired
-    private ApplicationContext context;
 
     @Autowired
     private UserDao dao;

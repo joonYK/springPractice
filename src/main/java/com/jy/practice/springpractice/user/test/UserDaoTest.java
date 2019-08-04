08bdc1a5e8e748b248a6ff787d0db594bd6d1115
JY <shutingz@naver.com>
2019-08-04 22:20:51 +0900

    @RunWith @ContextConfiguration 테스트 클래스에서 스프링 컨테이너를 한번만 생성

--- src/main/java/com/jy/practice/springpractice/user/test/UserDaoTest.java
+++ src/main/java/com/jy/practice/springpractice/user/test/UserDaoTest.java
@@ -5,30 +5,40 @@ import com.jy.practice.springpractice.user.domain.User;
 import org.hamcrest.CoreMatchers;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.sql.SQLException;
 
 import static org.junit.Assert.assertThat;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/applicationContext.xml")
 public class UserDaoTest {
 
+    @Autowired
+    private ApplicationContext context;
+
+    @Autowired
     private UserDao dao;
+
+    private User user1;
+    private User user2;
+    private User user3;
 
     @Before
     public void setUp() {
-        ApplicationContext context = new GenericXmlApplicationContext("/applicationContext.xml");
-        dao = context.getBean("userDao", UserDao.class);
+        user1 = new User("gyumee", "박성철", "springno1");
+        user2 = new User("leegw700", "이길원", "springno2");
+        user3 = new User("bumjin", "박범진", "springno3");
     }
 
     @Test
     public void addAndGet() throws SQLException {
-
-        User user1 = new User("gyumee", "박성철", "springno1");
-        User user2 = new User("leegw700", "이길원", "springno2");
-
         dao.deleteAll();
         assertThat(dao.getCount(), CoreMatchers.is(0));
 
@@ -47,10 +57,6 @@ public class UserDaoTest {
 
     @Test
     public void count() throws SQLException {
-        User user1 = new User("gyumee", "박성철", "springno1");
-        User user2 = new User("leegw700", "이길원", "springno2");
-        User user3 = new User("bumjin", "박범진", "springno3");
-
         dao.deleteAll();
         assertThat(dao.getCount(), CoreMatchers.is(0));
 

8f21e0c91b0b8ba86c02ce0a800ed06e99e72ba4
JY <shutingz@naver.com>
2019-08-07 22:42:37 +0900

    JUnit 테스트 개선

--- src/main/java/com/jy/practice/springpractice/learningtest/junit/JunitTest.java
+++ src/main/java/com/jy/practice/springpractice/learningtest/junit/JunitTest.java
@@ -1,31 +1,32 @@
 package com.jy.practice.springpractice.learningtest.junit;
 
 import org.junit.Test;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.sameInstance;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.hasItem;
 
 public class JunitTest {
-    static JunitTest testObject;
+    static Set<JunitTest> testObjects = new HashSet<JunitTest>();
 
     @Test
     public void test() {
-        assertThat(this, is(not(sameInstance(testObject))));
-        testObject = this;
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
     }
 
     @Test
     public void test2() {
-        assertThat(this, is(not(sameInstance(testObject))));
-        testObject = this;
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
     }
 
     @Test
     public void test3() {
-        assertThat(this, is(not(sameInstance(testObject))));
-        testObject = this;
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
     }
-
 }

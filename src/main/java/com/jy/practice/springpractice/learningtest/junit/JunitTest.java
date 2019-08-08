75d27adc2d52f454ba4f79d3de72c4f4cdd62969
JY <kims2606@gmail.com>
2019-08-08 12:51:05 +0900

    junit 테스트시에 context가 한번만 생성되는지 확인하는 학습테스트

--- src/main/java/com/jy/practice/springpractice/learningtest/junit/JunitTest.java
+++ src/main/java/com/jy/practice/springpractice/learningtest/junit/JunitTest.java
@@ -1,32 +1,50 @@
 package com.jy.practice.springpractice.learningtest.junit;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 import static org.junit.matchers.JUnitMatchers.hasItem;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/junit.xml")
 public class JunitTest {
+    @Autowired
+    ApplicationContext context;
+
     static Set<JunitTest> testObjects = new HashSet<JunitTest>();
+    static ApplicationContext contextObject = null;
 
     @Test
-    public void test() {
+    public void test1() {
         assertThat(testObjects, not(hasItem(this)));
         testObjects.add(this);
+        assertThat(contextObject == null || contextObject == this.context, is(true));
+        contextObject = this.context;
     }
 
     @Test
     public void test2() {
         assertThat(testObjects, not(hasItem(this)));
         testObjects.add(this);
+        assertTrue(contextObject == null || contextObject == this.context);
+        contextObject = this.context;
     }
 
     @Test
     public void test3() {
         assertThat(testObjects, not(hasItem(this)));
         testObjects.add(this);
+        assertThat(contextObject, either((is(nullValue()))).or(is(this.context)));
+        contextObject = this.context;
     }
 }

fd7bb30af499fd4235f7cd7a5829fbb31e5671fc
JY <kims2606@gmail.com>
2019-08-09 12:46:54 +0900

    deleteAll의 전략패턴 적용. 변하는부분과 변하지 않는 부분을 분리. DI적용 필요.

--- src/main/java/com/jy/practice/springpractice/user/dao/ConnectionMaker.java
+++
@@ -1,8 +0,0 @@
-package com.jy.practice.springpractice.user.dao;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface ConnectionMaker {
-    public Connection makeConnection() throws ClassNotFoundException, SQLException;
-}
---
+++ src/main/java/com/jy/practice/springpractice/user/dao/StatementStrategy.java
@@ -0,0 +1,9 @@
+package com.jy.practice.springpractice.user.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public interface StatementStrategy {
+    PreparedStatement makePreparedStatement(Connection c) throws SQLException;
+}

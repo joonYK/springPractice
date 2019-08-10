330cd85c6e5be11019a00154e93d9527415316bb
JY <shutingz@naver.com>
2019-08-10 13:29:26 +0900

    전략을 사용하는 context에서 전략의 구현체를 모르도록 메소드로 분리. 클라이언트에서 전략의 실제 구현체를 만들고 context로 DI (클라이언트와 컨텍스트를 분리)

--- src/main/java/com/jy/practice/springpractice/user/dao/UserDao.java
+++ src/main/java/com/jy/practice/springpractice/user/dao/UserDao.java
@@ -4,10 +4,7 @@ import com.jy.practice.springpractice.user.domain.User;
 import org.springframework.dao.EmptyResultDataAccessException;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class UserDao {
 
@@ -96,32 +93,8 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-
-        try {
-            c = dataSource.getConnection();
-
-            StatementStrategy strategy = new DeleteAllStatement();
-            ps = strategy.makePreparedStatement(c);
-
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if(ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {}
-            }
-
-            if(c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {}
-            }
-        }
-
+        StatementStrategy st = new DeleteAllStatement();
+        jdbcContextWithStatementStrategy(st);
     }
 
     public int getCount() throws SQLException {
@@ -147,6 +120,32 @@ public class UserDao {
                 } catch (SQLException e) {}
             }
 
+            if(ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {}
+            }
+
+            if(c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {}
+            }
+        }
+    }
+
+    private void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+
+        try {
+            c = dataSource.getConnection();
+            ps = stmt.makePreparedStatement(c);
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
             if(ps != null) {
                 try {
                     ps.close();

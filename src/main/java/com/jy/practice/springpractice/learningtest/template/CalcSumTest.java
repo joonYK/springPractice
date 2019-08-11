ce8616c1e6a278fa6763bcb6c7b9d507b6df5941
JY <shutingz@naver.com>
2019-08-11 16:49:13 +0900

    곱셈 메서드를 추가하면서 템플릿과 콜백을 분리. 파일을 읽고 계산로직을 수행할 콜백을 호출하는 템플릿 메서드 작성. 각 덧셈, 곱셈 메서드는 콜백 오브젝트를 생성하고 템플렛 메서드의 실행과 콜백을 DI

--- src/main/java/com/jy/practice/springpractice/learningtest/template/CalcSumTest.java
+++ src/main/java/com/jy/practice/springpractice/learningtest/template/CalcSumTest.java
@@ -1,5 +1,6 @@
 package com.jy.practice.springpractice.learningtest.template;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -7,11 +8,22 @@ import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 
 public class CalcSumTest {
+    private Calculator calculator;
+    private String numFilepath;
+
+    @Before
+    public void setUp() {
+        calculator = new Calculator();
+        numFilepath = getClass().getResource("numbers.txt").getPath();
+    }
 
     @Test
     public void sumOfNumbers() throws IOException {
-        Calculator calculator = new Calculator();
-        int sum = calculator.calcSum(getClass().getResource("numbers.txt").getPath());
-        assertThat(sum, is(10));
+        assertThat(calculator.calcSum(numFilepath), is(10));
+    }
+
+    @Test
+    public void multiplyOfNumbers() throws IOException {
+        assertThat(calculator.calcMultiply(numFilepath), is(24));
     }
 }
--- src/main/java/com/jy/practice/springpractice/learningtest/template/Calculator.java
+++ src/main/java/com/jy/practice/springpractice/learningtest/template/Calculator.java
@@ -6,17 +6,50 @@ import java.io.IOException;
 
 public class Calculator {
     public Integer calcSum(String filepath) throws IOException {
+        BufferedReaderCallback sumCallback = new BufferedReaderCallback() {
+            @Override
+            public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+                br = new BufferedReader(new FileReader(filepath));
+                Integer sum = 0;
+                String line = null;
+                while((line = br.readLine()) !=  null) {
+                    sum += Integer.parseInt(line);
+                }
+
+                return sum;
+            }
+        };
+
+        return fileReadTmeplate(filepath, sumCallback);
+
+    }
+
+    public Integer calcMultiply(String filepath) throws IOException {
+        BufferedReaderCallback multiplyCallback = new BufferedReaderCallback() {
+            @Override
+            public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+                br = new BufferedReader(new FileReader(filepath));
+                Integer multiply = 1;
+                String line = null;
+                while((line = br.readLine()) !=  null) {
+                    multiply *= Integer.parseInt(line);
+                }
+
+                return multiply;
+            }
+        };
+
+        return fileReadTmeplate(filepath, multiplyCallback);
+
+    }
+
+    public Integer fileReadTmeplate(String filepath, BufferedReaderCallback callback) throws IOException {
         BufferedReader br = null;
 
         try {
             br = new BufferedReader(new FileReader(filepath));
-            Integer sum = 0;
-            String line = null;
-            while((line = br.readLine()) !=  null) {
-                sum += Integer.parseInt(line);
-            }
+            return callback.doSomethingWithReader(br);
 
-            return sum;
         } catch (IOException e) {
             e.printStackTrace();
             throw e;

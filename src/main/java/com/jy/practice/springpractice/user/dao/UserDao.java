1eb6bf3df8eefdf116f450b7f05bd64457f52f86
JY <shutingz@naver.com>
2019-08-13 22:12:51 +0900

    getAll

--- src/main/java/com/jy/practice/springpractice/user/dao/UserDao.java
+++ src/main/java/com/jy/practice/springpractice/user/dao/UserDao.java
@@ -7,6 +7,7 @@ import org.springframework.jdbc.core.RowMapper;
 import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 public class UserDao {
 
@@ -48,4 +49,17 @@ public class UserDao {
         return jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
     }
 
+    public List<User> getAll() {
+        return jdbcTemplate.query("select * from users order by id",
+                new RowMapper<User>() {
+                    @Override
+                    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                        User user = new User();
+                        user.setId(rs.getString("id"));
+                        user.setName(rs.getString("name"));
+                        user.setPassword(rs.getString("password"));
+                        return user;
+                    }
+                });
+    }
 }

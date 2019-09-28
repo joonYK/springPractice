add2b4d81627179c81b6a47ab6bd35ed162d6a59
JY <shutingz@naver.com>
2019-09-28 23:20:25 +0900

    업그레이드 회원 메일 발송 기능 추가. 메일 서버의 존재유무, 테스트때마다 메일을 전송해야만 하는 문제가 있음. 테스트용 메일 전송 서비스가 필요.

--- src/main/java/com/jy/practice/springpractice/user/service/UserService.java
+++ src/main/java/com/jy/practice/springpractice/user/service/UserService.java
@@ -3,16 +3,22 @@ package com.jy.practice.springpractice.user.service;
 import com.jy.practice.springpractice.user.dao.UserDao;
 import com.jy.practice.springpractice.user.domain.Level;
 import com.jy.practice.springpractice.user.domain.User;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.DataSourceUtils;
+import com.sun.deploy.util.SessionProperties;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.sql.DataSource;
-import java.sql.Connection;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
+import java.util.Properties;
 
 public class UserService {
 
@@ -76,6 +82,7 @@ public class UserService {
     protected void upgradeLevel(User user) {
         user.upgradeLevel();
         userDao.update(user);
+        sendUpgradeEmail(user);
     }
 
     /**
@@ -87,5 +94,23 @@ public class UserService {
             user.setLevel(Level.BASIC);
 
         userDao.add(user);
+    }
+
+    private void sendUpgradeEmail(User user) {
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "mail.ksug.org");
+        Session session = Session.getInstance(props);
+
+        MimeMessage message = new MimeMessage(session);
+        try {
+            message.setFrom(new InternetAddress("useradmin@ksug.org"));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(user.getEmail()));
+            message.setSubject("Upgrade 안내");
+            message.setText("사용자님의 등급이 " + user.getLevel().name() + "로 업그레이드 되었습니다.");
+            
+            Transport.send(message);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/com/jy/practice/springpractice/user/test/UserDaoTest.java
+++ src/main/java/com/jy/practice/springpractice/user/test/UserDaoTest.java
@@ -37,9 +37,9 @@ public class UserDaoTest {
 
     @Before
     public void setUp() {
-        user1 = new User("gyumee", "박성철", "springno1", Level.BASIC, 1, 0);
-        user2 = new User("leegw700", "이길원", "springno2", Level.SILVER, 5, 10);
-        user3 = new User("bumjin", "박범진", "springno3", Level.GOLD, 100, 100);
+        user1 = new User("gyumee", "박성철", "springno1", Level.BASIC, 1, 0, "aaa@asd.com");
+        user2 = new User("leegw700", "이길원", "springno2", Level.SILVER, 5, 10, "bbb@bbb.com");
+        user3 = new User("bumjin", "박범진", "springno3", Level.GOLD, 100, 100, "shutingz@naver.com");
     }
 
     @Test
